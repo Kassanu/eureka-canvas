d125b3bc96c9e7290ff0668d495fb97976819c5e
Matt Martucciello <kassanu@gmail.com>
2020-07-22 18:15:06 -0400

    Basic drawing of icons and text for map positions
    adds a new prop that takes an array of map position objects
    
    This is the basic implementation, the object has an array of icons to draw and a label property for the txt to display
    
    No scaling or culling has been implemented yet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <EurekaCanvas :canvasImageSource="image" :gridSizeInPixels="50" :coordinatesOffset="1" />
+        <EurekaCanvas :canvasImageSource="image" :gridSizeInPixels="50" :coordinatesOffset="1" :positions="positions" />
     </div>
 </template>
 
@@ -14,14 +14,57 @@
         },
         data() {
             return {
-                image: require('./assets/test.jpg')
+                image: require('./assets/test.jpg'),
+                positions: []
             }
+        },
+        mounted() {
+            this.setUpPositions()
+        },
+        methods: {
+            setUpPositions() {
+                let positions = this.loadPositions()
+                positions.forEach(position => {
+                    position.icons.forEach(async (icon) => {
+                        icon.image =  await this.loadImage(icon.path)
+                    })
+                })
+
+                this.positions = positions
+            },
+            loadPositions() {
+                return [
+                    {
+                        label: 'Test',
+                        icons: [
+                            {
+                                path: require('./assets/testIconRed.png'),
+                                image: null
+                            },
+                            {
+                                path: require('./assets/testIconBlue.png'),
+                                image: null
+                            }
+                        ],
+                        coordinates: {
+                            x: 41,
+                            y: 41
+                        }
+                    }
+                ]
+            },
+            loadImage: url => new Promise(resolve => {
+                let img = new Image()
+                img.onload = () => resolve(img)
+                img.src = url
+            })
         }
     }
 </script>
 
 <style>
-    body, html {
+    body,
+    html {
         height: 100vh;
         width: 100vw;
         padding: 0;
